df65cd7c60c30bba2ea2c9a4b274ce277a72e39c
RZin <Roman__Z@mail.ru>
2018-07-28 17:10:44 +0100

    Add files via upload

--- sketch_robot_full.ino
+++ sketch_robot_full.ino
@@ -42,8 +42,8 @@ const int line_F_pin = A5;
 const int line_B_pin = A6;
 
 // task constants
-const float counting_freq = 1.0; //Hz set 10
-const float left_pulses_correction = 0.96;
+const float counting_freq = 10.0;
+const float left_pulses_correction = 0.96; // correction coeficient
 
 void setup() {
   // put your setup code here, to run once:
@@ -83,11 +83,11 @@ int sense_R () {
 }
 // 3.
 boolean sense_line_F () {
-  return (analogRead(line_F_pin)>500); // or without brackets
+  return (analogRead(line_F_pin)>500);
 }
 // 4.
 boolean sense_line_B () {
-  return (analogRead(line_B_pin)>500); // or without brackets
+  return (analogRead(line_B_pin)>500);
 }
 // 5. 
 void count_pulses(){
@@ -101,11 +101,6 @@ void count_pulses(){
     pulses_num_right = curr_pulses_num_Right - last_pulses_num_Right;
     NL = pulses_num_left*left_pulses_correction;
     NR = pulses_num_right;
-//    Serial.print(" NL ");
-//    Serial.print(NL);
-//    Serial.print(" NR ");
-//    Serial.print(NR);
-//    Serial.println();
     
     // update vars
     last_pulses_num_Left = curr_pulses_num_Left;
@@ -113,7 +108,6 @@ void count_pulses(){
     
     total_pulses_num_Left += abs(pulses_num_left);
     total_pulses_num_Right += abs(pulses_num_right);
-
 }
 
 void test_sensors(){
@@ -132,37 +126,32 @@ void test_sensors(){
   delay(5000);    
 }
 
-
 //
 // C O N T R O L
 //
-
 
 float CL=8000; // pulses per revolution
 float CR=8000;
 
 int t = 0;
 
-float N[10][2] =
-        {
-                {0,0},
-                {100,100},
-                {300,200},
-                {500,300},
-                {700,400},
-                {900,500},
-                {1000,600},
-                {1100,700},
-                {1200,800},
-        };
+//float N[10][2] =
+//        {
+//                {0,0},
+//                {100,100},
+//                {300,200},
+//                {500,300},
+//                {700,400},
+//                {900,500},
+//                {1000,600},
+//                {1100,700},
+//                {1200,800},
+//        };
 
 // positions 
-float x_curr;
-float x_prev;
-float y_curr;
-float y_prev;
-float theta_curr;
-float theta_prev;
+float x;
+float y;
+float theta;
 
 float D;
 
@@ -172,109 +161,87 @@ const float r = 0.015; // cm 0.015 mm 5
 
 float C=(CL+CR)/2; // not sure but for me 8000 was 100
 
-double VD = 0.0; // 0.08 max m/s
-double WD = 0.7; // 2 max rad/s
+float VD = 0.0; // 0.08 max m/s
+float WD = 0.7; // 2 max rad/s
 
-float VMAX = 0.08; // m/s
-float WMAX = 3.14/2; // rad/s PI*r half of round 1,57
+//float VMAX = 0.08; // m/s
+//float WMAX = 3.14/2; // rad/s PI*r half of round 1,57
 
-float WLR_MAX = 8.935;    // wheels max vel
 //define vels
-double v_curr;
-double w_curr;
-double v_prev=0;
-double w_prev=0;
+float v;
+float w;
 
-double wL_curr;
-double wR_curr;
+float wL;
+float wR;
 
 //define des vels
-double vd_curr;
-double wd_curr;
+float vd;
+float wd;
 
-double wLd_curr;
-double wRd_curr;
+float wLd;
+float wRd;
 
-void encUpdate(int t) {
-    NL = N[t][0]; // read
-    NR = N[t][1];
-}
+//void encUpdate(int t) {
+//    NL = N[t][0]; // read
+//    NR = N[t][1];
+//}
 
-const float delta_t = 1/counting_freq; // seconds
+const float delta_t = 1/counting_freq; // in seconds
 
 void cmd_vel(){
-    v_curr = (float)(NL+NR)*PI*r/C/delta_t;
-    w_curr = (float)(NL-NR)*PI*r/C/delta_t*2/b;
+    // calculate current velocities from pulses on encoders
+    v = (2*PI*r)*(NR+NL)/C/2/delta_t;
+    w = (2*PI*r)*(NR-NL)/C/b/delta_t;
 }
 
 void vel_to_wheels(float V, float W){
-    wL_curr = (float)(V-b*W/2)/r;
-    wR_curr = (float)(V+b*W/2)/r;
-    // 0.08/0.015 + 0.095*1,57/2/0.015 = 5,33 + 4,97 = 10.3 Hz
-    // mb wL_currMAX = 5.5
+    // convert current velocities to wheels' rot vels
+    wL = (float)(V-b*W/2)/r;
+    wR = (float)(V+b*W/2)/r;
 }
 
 void vel_to_wheels_desired(float V_des, float W_des){
+    // convert current desired velocities to desired wheels' rot vels
     wLd_curr = (float)(V_des-b*W_des/2)/r; 
     wRd_curr = (float)(V_des+b*W_des/2)/r;
 }
 
-
 void posUpdate(int NL, int NR, float r, float b, float C) {
+    // update position using pulses on encoders
 
+    // define displacements of the Wheels
     float DL = 2*PI*r*NL/CL;
     float DR = 2*PI*r*NR/CR;
+    float D = (DL+DR)/2;
 
-    float D=(float)(DL+DR)/2;
-
-    // not sure
-    x_curr = x_prev + D * cos(theta_prev) ;
-    y_curr = y_prev + D * sin(theta_prev) ;
-    theta_curr = theta_prev + (DR-DL)/b ;
-
-    // get v_curr, w_curr
-    cmd_vel();
-
-    // get wL_curr, wR_curr
-    vel_to_wheels(v_curr, w_curr); // wL_curr wR_curr
+    // update position
+    x = x + D * cos(theta) ;
+    y = y + D * sin(theta) ;
+    theta = theta + (DR-DL)/b ;
 }
-
 
 void calc_pose2(){
-    // get NL NR
+    // get encoder pulses NL NR
     count_pulses();
-//    Serial.print(" NL ");
-//    Serial.print(NL);
-//    Serial.print(" NR ");
-//    Serial.print(NR);
-//    Serial.println();
 
-    v_curr = (2*PI*r)*(NR+NL)/C/2/delta_t; // (2*3.14*0.015)*40000/8000/2/0.1
-    w_curr = (2*PI*r)*(NR-NL)/C/b/delta_t; // 
-//    Serial.print(" v_curr ");
-//    Serial.print(v_curr);    
-//    Serial.print(" w_curr ");
-//    Serial.print(w_curr);    
-//    Serial.println();
+    // update linear velocity and angular velocity
+    v = (2*PI*r)*(NR+NL)/C/2/delta_t; // (2*3.14*0.015)*40000/8000/2/0.1
+    w = (2*PI*r)*(NR-NL)/C/b/delta_t; //
 
-    theta_curr = atan2(sin(theta_prev + w_curr), cos(theta_prev + w_curr));
-    x_curr = x_prev + v_curr * cos(theta_curr);
-    y_curr = y_prev + v_curr * sin(theta_curr);
+    // update position and orientation
+    theta = atan2(sin(theta + w), cos(theta + w));
+    x = x + v * cos(theta); // should be *delta_t in seconds
+    y = y + v * sin(theta);
 }
 
+const int wheels_num = 2;
 // gain constants
-float G_curr[2]={0,0};
-
-// define errors at t=1
-float ep_curr[2];
+float G[2]={0, 0};
+// define errors
+float ep_curr[2] = {0, 0};
 float ep_prev[2] = {0, 0};
-
-float ei_curr[2];
-float ei_prev[2] = {0, 0};
-
-float ed_curr[2];
-float ed_prev[2] = {0, 0};
-
+float ei[2] = {0, 0};
+float ed[2] = {0, 0};
 
 void PID (){
     // coefs Kp
@@ -283,65 +250,68 @@ void PID (){
     float Kd = 1.0;
 
     // update errors
-    // We have wL_curr wR_curr
+    // We have wL wR
 
+    // save prev
+    ep_prev = ep_curr;
     // left proportional
-    ep_curr[0] = wLd_curr - wL_curr; // hz
+    ep_curr[0] = wLd - wL; // in hz
     // right proportional
-    ep_curr[1] = wRd_curr - wR_curr;
+    ep_curr[1] = wRd - wR;
     // left integral
-    ei_curr[0] = (ei_prev[0] + ep_curr[0])*delta_t; // delta_t or 1
+    ei[0] = (ei[0] + ep_curr[0])*delta_t;
     // right integral
-    ei_curr[1] = (ei_prev[1] + ep_curr[1])*delta_t; // delta_t or 1
+    ei[1] = (ei[1] + ep_curr[1])*delta_t;
     // left derivative
-    ed_curr[0] = (ep_curr[0] - ep_prev[0])/delta_t;
+    ed[0] = (ep_curr[0] - ep_prev[0])/delta_t;
     // right derivative
-    ed_curr[1] = (ep_curr[1] - ep_prev[1])/delta_t;
+    ed[1] = (ep_curr[1] - ep_prev[1])/delta_t;
 
-    // get the gain
-//    G_prev = G_curr; // hz 
-        
-    G_curr[0] = G_curr[0] + Kp*ep_curr[0] + Ki*ei_curr[0] + Kd*ed_curr[0]; // hz 
-    G_curr[1] = G_curr[1] + Kp*ep_curr[1] + Ki*ei_curr[1] + Kd*ed_curr[1];
-    
+    // update gain based on errors
+    G[0] = G[0] + Kp*ep_curr[0] + Ki*ei[0] + Kd*ed[0]; // hz
+    G[1] = G[1] + Kp*ep_curr[1] + Ki*ei[1] + Kd*ed[1];
+}
 
+void PID_spare (){
+    // coefs Kp
+    float Kp = 30.0;
+    float Ki = 1.8;
+    float Kd = 1.0;
 
-    
+    // update errors
+    // We have wL wR
+
+    // save prev
+    ep_prev = ep_curr;
+
+    for (wheel=0; wheel<wheels_num; wheel++) {
+        ep_curr[wheel] = wd[wheel] - w_curr[wheel]; // in hz
+        // integral
+        ei[wheel] = (ei[wheel] + ep_curr[wheel])*delta_t;
+        //  derivative
+        ed[wheel] = (ep_curr[wheel] - ep_prev[wheel])/delta_t;
+        // update gain based on errors
+        G[wheel] = G[wheel] + Kp*ep_curr[wheel] + Ki*ei[wheel] + Kd*ed[wheel]; // hz
+    }
 }
 
 void encFunc() {t++;}
 
 void pid_controller(){
-    // 1. set desired
+    // 1. set desired speed
 //    VD = 0.03; // by now constant m/s
 //    WD = 0.0;
     // 2. get wLd_curr wRd_curr
-    vel_to_wheels_desired(VD, WD); // updates wLd_curr wRd_curr
-//    Serial.print(" wLd_curr ");
-//    Serial.print(wLd_curr);    
-//    Serial.print(" wRd_curr ");
-//    Serial.print(wRd_curr);    
-//    Serial.println();
+    vel_to_wheels_desired(VD, WD); // updates wLd wRd
+
     // 3. Run algorithm Calculate Pose 2
     calc_pose2(); // get v w theta x y
 
-    
     // 4. Calculate the real wL, wR
-    vel_to_wheels(v_curr, w_curr); // updates wL_curr wR_curr
-//    Serial.print(" wL_curr ");
-//    Serial.print(wL_curr);    
-//    Serial.print(" wR_curr ");
-//    Serial.print(wR_curr);    
-//    Serial.println();
+    vel_to_wheels(v, w); // updates wL wR
+
     // run PID
-    PID(); // gets GL GR
-//    Serial.print(" GL_curr ");
-//    Serial.print(G_curr[0]);
-//    Serial.print(" GR_curr ");
-//    Serial.print(G_curr[1]);
-//    Serial.println();
-      
-// use limit for G
+    PID(); // gets G[2]
 
 }
 
@@ -414,7 +384,6 @@ void move_at(float speed_left, float speed_right) {
       Serial.print(speed_right);
       Serial.println();
       }
-
 
 //    Serial.print(" speed_left ");  
 //    Serial.print(speed_left);  
